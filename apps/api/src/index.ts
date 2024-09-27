786ddde160c67575822d8fb6fc5211b18046ec32
Firas Jaber <firasjaberc@gmail.com>
2024-09-27 22:13:00 +0200

    link router

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,15 +2,12 @@ import { Elysia } from 'elysia';
 import { users } from './user/route';
 import cors from '@elysiajs/cors';
 import { initDrizzle } from 'db';
+import { links } from './link/route';
 
 export const drizzle = await initDrizzle();
 console.log('🐘 Database connected');
 
-const app = new Elysia()
-  .use(cors())
-  .use(users)
-  .get('/', () => 'Hello Elysia')
-  .listen(3000);
+const app = new Elysia().use(cors()).use(users).use(links).listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
 
