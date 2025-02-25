23b1699baf3dd97ffb579cfc1d1da46353b59ab5
Firas Jaber <firasjaberc@gmail.com>
2025-02-25 21:36:50 +0100

    feat: google oauth routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,22 @@
-import { Elysia } from 'elysia';
-import { users } from './user/route';
-import cors from '@elysiajs/cors';
-import { initDrizzle } from 'db';
-import { links } from './link/route';
+import { Elysia } from "elysia";
+import { users } from "./user/route";
+import cors from "@elysiajs/cors";
+import { initDrizzle } from "db";
+import { links } from "./link/route";
+import { googleAuth } from "./auth/google.route";
 
 export const drizzle = await initDrizzle();
-console.log('🐘 Database connected');
+console.log("🐘 Database connected");
 
-const app = new Elysia().use(cors()).use(users).use(links).listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .use(users)
+  .use(links)
+  .use(googleAuth)
+  .listen(3000);
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
 
 export type App = typeof app;
